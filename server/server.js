40d292cfd78394eb3bd9702920fc29a6d3e00fa7
Piervalerio Vignola <Piervalerio@MacBook-Air-di-Piervalerio.local>
2018-01-02 18:38:30 -0500

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.get('/todos/:id', (req, res) => {
     }
     Todo.findById(id).then((todo) => {
         if(!todo){
-            res.status(400).send('No todo found');
+            res.status(404).send('No todo found');
         }
         res.send({todo});
         }).catch(() => {
