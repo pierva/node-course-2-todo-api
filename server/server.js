a165022009a39af3ad2db12ebac193aee3132e1b
Piervalerio Vignola <Piervalerio@MacBook-Air-di-Piervalerio.local>
2018-01-09 19:20:54 -0500

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -91,6 +91,19 @@ app.patch('/todos/:id' , (req, res) => {
     })
 })
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
 
 app.listen(port, () => { //for localhost change "port" with 3000
     // console.log('Started on port 3000'); //Used for localhost
