84e95d2fd8f9125fd415fbab0823ca1d8404c4c4
Piervalerio Vignola <Piervalerio@MacBook-Air-di-Piervalerio.local>
2018-01-28 10:59:14 -0500

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -123,6 +123,14 @@ app.post('/users/login', (req, res) => {
     });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    })
+});
+
 
 app.listen(port, () => { //for localhost change "port" with 3000
     // console.log('Started on port 3000'); //Used for localhost
